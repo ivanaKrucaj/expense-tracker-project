09173a802429bb590a492a82493e6b2d257fbbe9
Justine Benevent <justinebnvent@gmail.com>
2020-06-04 15:34:01 +0300

    user and transaction models created fixes #4

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -1,1 +1,47 @@
+const { Schema, model } = require("mongoose");
 
+require("mongoose-currency").loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+const transactionSchema = new Schema(
+  {
+    type: {
+      type: String,
+      required: [true, "Please select a type"],
+      enum: ["expense", "income"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter a name"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please select a category"],
+      enum: [
+        "food",
+        "transport",
+        "accomodation",
+        "salary",
+        "travel",
+        "bills",
+        "entertainment",
+        "clothing",
+        "education",
+        "healthcare",
+      ],
+    },
+    amount: {
+      type: Number,
+      required: [true, "Please enter an amount"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Please enter a date"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Transaction", transactionSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,32 @@
+const { Schema, model } = require("mongoose");
 
+require("mongoose-currency").loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please enter username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+      unique: [true, "Email registered. Please use a new one"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    currency: {
+      amount: { type: Currency },
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
