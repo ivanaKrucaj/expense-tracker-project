738b9dd441985ab9fabe2217f2f03ebd29ac3ebb
Justine Benevent <justinebnvent@gmail.com>
2020-06-04 12:10:59 +0300

    deployment changes

---
+++ models/configs/db.config.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URI;
+// mongodb+srv://justinebenevent:<password>@cluster0-jo7rd.mongodb.net/<dbname>?retryWrites=true&w=majority
