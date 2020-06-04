fda79374193dc2e14bcaa4c9fb022ba7fce5e1f0
Justine Benevent <justinebnvent@gmail.com>
2020-06-04 13:57:00 +0300

    set up fixes #2 fixes #1 fixes #3

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
 
+//A library that helps us log the requests in the console
+const logger = require("morgan");
+
+// Used to setthe favicon for our app
+const favicon = require("serve-favicon");
+
+const cookieParser = require("cookie-parser");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+
+// Set up the database
+require("./configs/db.config");
+
+// Routers
+const indexRouter = require("./routes/index.routes");
+
+const app = express();
+
+// Express View engine setup
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+
+// setting up the middleware to let it know where to find the favicon icon
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// Sets up morgan in our middleware so that we can see the requests getting logged
+app.use(logger("dev"));
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+app.use(
+  session({
+    secret: "our-secret-project",
+    saveUninitialized: false,
+    resave: true,
+    cookie: {
+      maxAge: 60 * 60 * 24 * 1000, //60 sec * 60 min * 24hrs = 1 day (in milliseconds)
+    },
+    store: new MongoStore({
+      url: "mongodb://localhost/basicAuth",
+      // mongooseConnection: mongoose.connection
+      //time to live (in seconds)
+      ttl: 60 * 60 * 24,
+      autoRemove: "disabled",
+    }),
+  })
+);
+
+// a body parser to allow us to parse form submissions
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// helps us use the cookies from each request
+app.use(cookieParser());
+
+// Routes middleware
+app.use("/", indexRouter);
+
+module.exports = app;
--- models/configs/db.config.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI;
-// mongodb+srv://justinebenevent:<password>@cluster0-jo7rd.mongodb.net/<dbname>?retryWrites=true&w=majority
