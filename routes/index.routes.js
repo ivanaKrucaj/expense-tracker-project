a5135315779d816a16aa92ffa06e034385def255
Justine Benevent <justinebnvent@gmail.com>
2020-06-04 16:11:00 +0300

    create landing page fixes #8 signin page route fixes #6 signup page route fixes #11 fixes #5

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,4 +4,8 @@ const router = express.Router();
 /* GET home page */
 router.get("/", (req, res) => res.render("index"));
 
+router.get("/signup", (req, res) => res.render("auth/signup"));
+
+router.get("/signin", (req, res) => res.render("auth/signin"));
+
 module.exports = router;
