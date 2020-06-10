1640d725cd3cf83890ae5fa34b1ceaace7202502
Ivana Krucaj <ivana.krucaj@gmail.com>
2020-06-10 13:02:37 +0200

    deploy to heroku

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGODB_URI =
-  process.env.MONGODB_URI;
+  process.env.MONGODB_URI || "mongodb://localhost/expense-tracker";
 
 mongoose
   .connect(MONGODB_URI, {
